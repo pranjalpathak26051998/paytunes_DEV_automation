ca04d6cbfb98b015fcb8293f8b7d34432d883b4c
Pranjal-Paytunes1432 <pranjal.p@paytunes.in>
2025-02-07 18:30:29 +0530

    syncRO test cases

--- test/loginAndSyncRO.js
+++ test/loginAndSyncRO.js
@@ -19,28 +19,43 @@ const {
     selectBrandName,
     selectSalesIncharge,
     enterSalesInchargeComment,
-    clickSaveAndContinue
+    clickSaveAndContinue,
+    clickViewSite,
+    clickOrder,
+    seeAllROs,
+    syncRO
 } = require('../pages/loginPaytunes2');
 let driver;
 
 describe("Login and Sync RO on Paytunes", () => {
-    before(async function () {
-        driver = await setupDriver();
-        console.log("Driver setup is complete");
-    });
-    after(async function () {
-        await closeDriver();
-        console.log("Driver closed");
-    });
+    try {
+        before(async function () {
+            driver = await setupDriver();
+            console.log("Driver setup is complete");
+        });
+        after(async function () {
+            await closeDriver();
+            console.log("Driver closed");
+        });
 
-    //access the website
-    it("should access the website", async function () { await accessWebsite(); console.log("Accessed website successfully"); });
-    //enter the user name
-    it("should enter username", async function () { await enterUsername(); console.log("Username Entered successfully"); });
-    //enter the password
-    it("should enter the password", async function (){ await enterPassword(); console.log("Password entered successfully");});
-    //click on the login button to login
-    it("should click the login button", async function(){ await clickLogin(); console.log("Login button clicked successfully"); });
+        //access the website
+        it("should access the website", async function () { await accessWebsite(); console.log("Accessed website successfully"); });
+        //enter the user name
+        it("should enter username", async function () { await enterUsername(); console.log("Username Entered successfully"); });
+        //enter the password
+        it("should enter the password", async function () { await enterPassword(); console.log("Password entered successfully"); });
+        //click on the login button to login
+        it("should click the login button", async function () { await clickLogin(); console.log("Login button clicked successfully"); });
+        //click on Order
+        it("should click the order ", async function(){await clickOrder(); console.log("Order clicked successfully")});
+        //click on see all ROs
+        it("should click on see all ROs",async function(){await seeAllROs(); console.log("Clicked on See All ROs successfully")});
+        //click on sync RO button
+        it("should click on sync RO button", async function(){await syncRO(); console.log("Clicked on Sync RO successfully")});
 
+    } catch (error) {
+        console.log(error);
+
+    }
 });
-    
+
